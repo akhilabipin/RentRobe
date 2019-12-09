476528e82a754d6d33cabc318a203ffbcf5b5b6c
manoj <akhilaraj20@gmail.com>
2019-12-09 13:31:03 +0530

    first commit

---
+++ app/src/main/java/com/example/rentrobe/activity/EntermobActivity.java
@@ -0,0 +1,7 @@
+package com.example.rentrobe.activity;
+
+public class EntermobActivity {
+
+
+
+}
---
+++ app/src/main/java/com/example/rentrobe/activity/RegistrationActivity.java
@@ -0,0 +1,18 @@
+package com.example.rentrobe.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private Context context;
+
+
+
+
+
+
+}
